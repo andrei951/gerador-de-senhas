6e84432d9d3877c68bef3b551d5223faab313c8e
andrei951 <106329603+andrei951@users.noreply.github.com>
2024-08-15 09:16:53 -0300

    arquivo js criado

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+const numerosenha = document.querySelector('.parametro-senha__texto');
+let tamanhosenha = 12;
+numerosenha.textContent = tamanhosenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll(.parametro-senha__botao);
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const 
